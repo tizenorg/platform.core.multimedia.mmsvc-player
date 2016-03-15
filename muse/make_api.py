25425a5d8effa21d8a0b1a3fdc347231c6c0bff8
Hyongtaek Lim <hyongtaek.lim@samsung.com>
2016-03-15 19:11:46 +0900

    auto-get API phase2 (make body template)
    Signed-off-by: Hyongtaek Lim <hyongtaek.lim@samsung.com>
    Change-Id: I9343c537fc33e4e4ec85c0054639f729533b5b3b

--- muse/make_api.py
+++ muse/make_api.py
@@ -1,21 +1,87 @@
 #!/usr/bin/env python
-import sys, getopt
+import sys, getopt, os.path
 
-def main(argv):
-	product = ""
-	try:
-		opts, args = getopt.getopt(argv, "hpl:", ["product","list"])
-	except getopt.GetoptError:
-		print 'make_api.py -l api.list [-p]'
-		sys.exit(1)
-	for opt, arg in opts:
-		if opt == '-h':
-			print 'make_api.py -l api.list [-p]'
-			sys.exit(0)
-		elif opt in ("-l", "--list"):
-			ListFileName = arg
-		elif opt in ("-p", "--product"):
-			product = "product_"
+def usage(ret):
+	print "NAME"
+	print "\tAuto-gen muse daemon API"
+	print "\nSYNOPSIS"
+	print "\tpython make_api.py -l LISTFILE [OPTION]..."
+	print "\nDESCRIPTION"
+	print "\t-l"
+	print "\t\tLISTFILE is api list. mandatory option. make enum, dispatcher and header based this file"
+	print "\t-p, --product"
+	print "\t\tuse for product API"
+	print "\t-s SOURCEFILE, --server SOURCEFILE"
+	print "\t\tmake body template for mused module(server). must use with -c option"
+	print "\t-c SOURCEFILE, --client SOURCEFILE"
+	print "\t\tmake body template for capi(client). must use with -s option"
+	print "\nEXAMPLE"
+	print "\tpython make_api.py -l api.list"
+	print "\t\tbuild time script"
+	print "\tpython make_api.py -l api.list -s src/muse_player.c -c ../../api/player/src/player.c"
+	print "\t\tmake body template"
+
+	sys.exit(ret)
+
+def make_body(ListFileName, ServerFileName, ClientFileName):
+	if not (os.path.isfile(ListFileName) and os.path.isfile(ServerFileName) and os.path.isfile(ClientFileName)):
+		print("File is not foundi!!\n")
+		usage(1)
+
+	print("Input new API")
+	if sys.version_info >= (3,0):
+		ApiName = input("player_")
+	else:
+		ApiName = raw_input("player_")
+	ApiName = ApiName.strip()
+
+	with open(ListFileName, "r") as ListFile:
+		ApiList = ListFile.readlines()
+		for Api in ApiList:
+			if Api.strip() == ApiName:
+				print("API already exist\n")
+				sys.exit(1)
+
+	with open(ListFileName, "a") as ListFile:
+		ListFile.write(ApiName + '\n')
+
+
+	with open(ServerFileName, "a") as ServerFile:
+		ServerFile.write("\n")
+		ServerFile.write("int player_disp_" + ApiName + "(muse_module_h module)\n")
+		ServerFile.write("{\n")
+		ServerFile.write("\tint ret = -1;\n")
+		ServerFile.write("\tintptr_t handle;\n")
+		ServerFile.write("\tmuse_player_api_e api = MUSE_PLAYER_API_" + ApiName.upper() +";\n")
+		ServerFile.write("\n")
+		ServerFile.write("\thandle = muse_core_ipc_get_handle(module);\n")
+		ServerFile.write("\n")
+		ServerFile.write("\tret = ACTUAL_OPERATION_FUNCTION((player_h)handle);\n")
+		ServerFile.write("\n")
+		ServerFile.write("\tplayer_msg_return(api, ret, module);\n")
+		ServerFile.write("\n")
+		ServerFile.write("\treturn ret;\n")
+		ServerFile.write("}\n")
+
+	with open(ClientFileName, "a") as ClientFile:
+		ClientFile.write("\n")
+		ClientFile.write("int player_" + ApiName + "(player_h player)\n")
+		ClientFile.write("{\n")
+		ClientFile.write("\tPLAYER_INSTANCE_CHECK(player);\n")
+		ClientFile.write("\tint ret = PLAYER_ERROR_NONE;\n")
+		ClientFile.write("\tmuse_player_api_e api = MUSE_PLAYER_API_" + ApiName.upper() +";\n")
+		ClientFile.write("\tplayer_cli_s *pc = (player_cli_s *)player;\n")
+		ClientFile.write("\n")
+		ClientFile.write("\tchar *ret_buf = NULL;\n")
+		ClientFile.write("\n")
+		ClientFile.write("\tplayer_msg_send(api, pc, ret_buf, ret);\n")
+		ClientFile.write("\n")
+		ClientFile.write("\tg_free(ret_buf);\n")
+		ClientFile.write("\n")
+		ClientFile.write("\treturn ret;\n")
+		ClientFile.write("}\n")
+
+def build_running(ListFileName, product):
 
 	DefFileName = "include/muse_player_" + product + "api.def"
 	FuncFileName = "include/muse_player_" + product + "api.func"
@@ -38,7 +104,7 @@ def main(argv):
 	HeadFile.write(HeaderPrefix)
 
 	for line in ListFile:
-		line = line.replace('\n', '')
+		line = line.strip()
 		if len(line) > 1:
 			DefFile.write("MUSE_PLAYER_API_" + line.upper() +",\n")
 			FuncFile.write("player_disp_" + line + ",\n")
@@ -50,6 +116,39 @@ def main(argv):
 	ListFile.close()
 	FuncFile.close()
 	HeadFile.close()
+
+def main(argv):
+	product = ""
+	make_body_opt = 0
+	ServerFileName = ""
+	ClientFileName = ""
+	ListFileName = ""
+	try:
+		opts, args = getopt.getopt(argv, "hpl:s:c:", ["product","list","server","client"])
+	except getopt.GetoptError:
+		usage(1)
+	for opt, arg in opts:
+		if opt == '-h':
+			usage(0)
+		elif opt in ("-l", "--list"):
+			ListFileName = arg
+		elif opt in ("-p", "--product"):
+			product = "product_"
+		elif opt in ("-s", "--server"):
+			make_body_opt = make_body_opt + 1
+			ServerFileName = arg
+		elif opt in ("-c", "--client"):
+			make_body_opt = make_body_opt + 1
+			ClientFileName = arg
+
+	if not ListFileName:
+		usage(1)
+	elif make_body_opt >= 2:
+		make_body(ListFileName, ServerFileName, ClientFileName)
+	elif make_body_opt:
+		usage(1)
+	else:
+		build_running(ListFileName, product)
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
