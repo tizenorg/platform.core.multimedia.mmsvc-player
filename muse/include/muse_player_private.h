5c389efbcb360bd4d7bd923d498a694b2019da4b
Eunhae Choi <eunhae1.choi@samsung.com>
2016-04-21 15:41:06 +0900

    change buffer release routine
    1. add muse_player struct.
    2. keep the list of exported media_packet to prevent mem leak.
       if there are remained packets, it will be handled during player_destroy.
    3. add handling _API_RETURN_BUFFER msg to release tbm bo.
    
    Change-Id: Iec71efbe9e2301646e9f296e9c8ce76436d0b873
    Signed-off-by: Eunhae Choi <eunhae1.choi@samsung.com>

---
+++ muse/include/muse_player_private.h
@@ -0,0 +1,53 @@
+/*
+* Copyright (c) 2011 Samsung Electronics Co., Ltd All Rights Reserved
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef __TIZEN_MEDIA_MUSE_PLAYER_PRIVATE_H__
+#define	__TIZEN_MEDIA_MUSE_PLAYER_PRIVATE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <tbm_bufmgr.h>
+#include <muse_core.h>
+#include "legacy_player.h"
+
+typedef struct {
+	player_h player_handle;
+	tbm_bufmgr bufmgr;
+	media_format_h audio_format;
+	media_format_h video_format;
+	GList *packet_list;
+	GList *data_list;
+	GMutex list_lock;
+} muse_player_handle_s;
+
+typedef struct {
+	tbm_bo bo;
+	int key;
+//	void *internal_buffer;
+} muse_player_export_data_s;
+
+typedef struct {
+	player_h player;
+	muse_module_h module;
+} prepare_data_s;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __TIZEN_MEDIA_MUSE_PLAYER_PRIVATE_H__ */
